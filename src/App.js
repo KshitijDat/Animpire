796197dcd55d2cdf3e337523d09f26dc5bd8a6c0
Saurabh Datta <saurabhdatta222001@gmail.com>
2023-08-24 22:05:30 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import AniCard from "./AniCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [animes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    searchAnimes("One Piece");
+  }, []);
+
+  const searchAnimes = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setAnimes(data.Search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Animpire</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for animes"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchAnimes(searchTerm)}
+        />
+      </div>
+
+      {animes?.length > 0 ? (
+        <div className="container">
+          {animes.map((animes) => (
+            <AniCard anime={animes} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No animes found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
